10cc490740256a32f47b0d9471a2b26736ef06f8
vital2190 <vital2190@DESKTOP-1C56GVG>
2023-06-14 00:11:38 +0500

    add bin/

---
+++ bin/gendiff.js
@@ -0,0 +1,18 @@
+#!/usr/bin/env node
+import { Command } from 'commander';
+//import genDiff from '../src/index.js';
+import genDiff from '../src/getDiff.js';
+
+const program = new Command();
+
+program
+  .description('Compares two configuration files and shows a difference.')
+  .version('0.0.1', '-v, --version', 'output the version number')
+  .option('-f, --format <type>', 'output format')
+  .arguments('<filepath1> <filepath2>')
+  .action((filepath1, filepath2) => {
+    console.log(genDiff(filepath1, filepath2));
+    //   //console.log(filepath1, filepath2);
+  });
+
+program.parse();
