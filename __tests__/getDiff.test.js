21991cc1799b521433db7bb850c7b53473a442a2
vital2190 <vital2190@DESKTOP-1C56GVG>
2023-06-17 01:17:36 +0500

    fix __tests__/getDiff.test.js

--- __tests__/getDiff.test.js
+++ __tests__/getDiff.test.js
@@ -8,11 +8,11 @@ import genDiff from '../src/getDiff.js';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
-//const readFile = (filename) => readFileSync(getFixturePath(filename), 'utf-8');
+//  const readFile = (filename) => readFileSync(getFixturePath(filename), 'utf-8');
 
 const jsonDiff = genDiff(getFixturePath('file1.json'), getFixturePath('file2.json'));
 const resultExpected = (filename) => readFileSync(getFixturePath(filename), 'utf-8');
 
 test('genDiffJsonFile', () => {
-    expect(jsonDiff).toEqual(resultExpected('jsonTest.txt'));
+  expect(jsonDiff).toEqual(resultExpected('jsonTest.txt'));
 });
