195734eff1d861033794f4f2ec7f4fc5e2e2f13d
vital2190 <vital2190@DESKTOP-1C56GVG>
2023-06-29 23:11:38 +0500

    fix __tests__/genDiff.test.js

---
+++ __tests__/genDiff.test.js
@@ -0,0 +1,24 @@
+import { describe, expect, test } from '@jest/globals';
+import { fileURLToPath } from 'url';
+import path, { dirname } from 'path';
+import { readFileSync } from 'fs';
+
+import genDiff from '../src/index.js';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
+
+describe.each([['stylish'], ['plain'], ['json']])('%s formatter', (formatter) => {
+  const filepathOfExpected = getFixturePath(`${formatter}Test.txt`);
+  const expected = readFileSync(filepathOfExpected, 'utf-8');
+
+  test.each([['json'], ['yml']])('%s files', (extension) => {
+    const filepath1 = getFixturePath(`file1.${extension}`);
+    const filepath2 = getFixturePath(`file2.${extension}`);
+
+    const result = genDiff(filepath1, filepath2, formatter);
+    
+    expect(result).toEqual(expected);
+  });
+});
